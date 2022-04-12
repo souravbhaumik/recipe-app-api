146f7a4c149816116edaabd45231f35590041b0a
Sourav <souravbhaumik0812@gmail.com>
2022-04-13 02:53:53 +0530

    Completed Custom User Model

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,46 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@email.com",
+            password="password123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@email.com",
+            password="password123",
+            name="Test user full name"
+        )
+
+    def test_user_listed(self):
+        """
+            Tests that user is listed on user page
+        """
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_edit_page(self):
+        """
+            Tests that the user edit page works
+        """
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """
+            Tests that the create user page works
+        """
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
